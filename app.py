860bc175ff4202a97bfe37ff5c473690886db534
Madhavan <madhavan.r@whiteblink.com>
2026-02-18 22:05:27 +0530

    Rebase feature commit

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 print('Hello from Git Assessment')
 print('Feature branch change')
+<<<<<<< HEAD
 print('Main branch update')
+=======
+print('Rebase change')
+>>>>>>> 844be77 (Rebase feature commit)
